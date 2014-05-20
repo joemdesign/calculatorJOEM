e7d27952489178a5212ae7067b472d3d0deda704
Joe Morris <joem@joemdesign.com>
2014-05-20 14:43:35 -0400

    Initial Commit

---
+++ calculatorJOEM/joemAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  joemAppDelegate.h
+//  calculatorJOEM
+//
+//  Created by Joe Morris on 5/20/14.
+//  Copyright (c) 2014 joemdesign, llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface joemAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ calculatorJOEM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  calculatorJOEM
+//
+//  Created by Joe Morris on 5/20/14.
+//  Copyright (c) 2014 joemdesign, llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "joemAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([joemAppDelegate class]));
+    }
+}
